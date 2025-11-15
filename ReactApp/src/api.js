abd899e44d84a79521fb4d7a80e08664c7e6d381
Nathan <G00414780@atu.ie>
2025-11-15 19:33:51 +0000

    Working test version of the game - displays random questions on the webpage with 0 and infinity

--- ReactApp/src/api.js
+++ ReactApp/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL = "/api";
+const API_BASE_URL = "/api"; //Allows to easily change the URL in the future if needed
 
 export async function fetchRandomQuestion() {
   const res = await axios.get(`${API_BASE_URL}/questions/random`);  //in main -> @app.get("/api/questions/random", response_model=QuestionReadPublic)
